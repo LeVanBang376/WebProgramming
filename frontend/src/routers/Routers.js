e8f1720f3caf122fa7c137a2c50cc25010f3208b
LeVanBang376 <levanbang376@gmail.com>
2022-12-01 17:45:03 +0700

    finish merge 1_12

--- frontend/src/routers/Routers.js
+++ frontend/src/routers/Routers.js
@@ -5,7 +5,7 @@ import Introduction from '../pages/Introduction/Introduction'
 import Contact from '../pages/Contact/Contact'
 import News from '../pages/News/News'
 import Login from '../pages/Login/Login'
-
+import Cart from '../pages/Cart/Cart'
 export default function Routers() {
     return (
         <Routes>
@@ -14,6 +14,7 @@ export default function Routers() {
             <Route path="/News" element={<News />} />
             <Route path="/Contact" element={<Contact />} />
             <Route path="/Login" element={<Login />} />
+            <Route path="/Cart" element={<Cart />} />
         </Routes>
     )
 }
