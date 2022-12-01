ae68ef18bef89c75afe589fa374f735ba6116394
LeVanBang376 <levanbang376@gmail.com>
2022-12-01 17:24:07 +0700

    merge with bang_coder

---
+++ frontend/src/routers/Routers.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '../pages/Home/Home'
+import Introduction from '../pages/Introduction/Introduction'
+import Contact from '../pages/Contact/Contact'
+import News from '../pages/News/News'
+import Login from '../pages/Login/Login'
+
+export default function Routers() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Introduction" element={<Introduction />} />
+            <Route path="/News" element={<News />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Login" element={<Login />} />
+        </Routes>
+    )
+}
