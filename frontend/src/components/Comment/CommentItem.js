490ad96c1c504d7f2e0007165edb9dfcb682a27e
LeVanBang376 <levanbang376@gmail.com>
2022-12-01 17:45:03 +0700

    finish merge 1_12

---
+++ frontend/src/components/Comment/CommentItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Comment, Form } from 'semantic-ui-react'
+
+const CommentItem = (props) => {
+    let { author, imgUrl, date, content } = props;
+    return (
+        <Comment.Group>
+            <Comment>
+                <Comment.Avatar as='a' src={imgUrl} />
+                <Comment.Content>
+                    <Comment.Author>{author}</Comment.Author>
+                    <Comment.Metadata>
+                        <div>{date}</div>
+                    </Comment.Metadata>
+                    <Comment.Text>
+                        {content}
+                    </Comment.Text>
+                    <Comment.Actions>
+                        <Comment.Action>Reply</Comment.Action>
+                    </Comment.Actions>
+                </Comment.Content>
+            </Comment>
+
+        </Comment.Group>
+    )
+}
+export default CommentItem
